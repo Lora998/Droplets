a8aaf2c1da38171b2351adb132650f41eedd0c62
l.hofer <l.hofer@iek3100.iek.kfa-juelich.de>
2018-06-08 10:20:27 +0200

    Main

---
+++ src/CreateArea.cpp
@@ -0,0 +1,15 @@
+/*
+ * CreateArea.cpp
+ *
+ *  Created on: June 09, 2018
+ *      Author: l.hofer
+ */
+ 
+ #include "Area.h"
+ #include <iostream>
+ using namespace std;
+ 
+ int main(){
+ 	cout << "Test" << endl;
+ 	return 0;
+ }
